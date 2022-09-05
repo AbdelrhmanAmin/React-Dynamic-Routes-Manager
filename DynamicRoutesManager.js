df63825778fbc0283b22b3b6bea2a56486cd166a
Abdo Amin <abdo.amin9991@gmail.com>
2022-09-05 20:20:48 +0200

    Update DynamicRoutesManager.js

--- DynamicRoutesManager.js
+++ DynamicRoutesManager.js
@@ -3,51 +3,27 @@
 // So I decided to create a wrapper component to manager to fetch the server and check  if the id is valid. You can tweak this code to your own needs..
 
 export const DynamicRouteManager = ({ children, fetcher }) => {
-  const flags = useRef({
-    isFirstRender: true,
-    didValidate: false,
-  });
-  const invalidRoutes = useRef([]);
   const params = useParams();
-  const { pathname } = useLocation();
   const [isLoading, setIsLoading] = useState(true);
   const [isValid, setIsValid] = useState(false);
-  const fetchToValidateRoute = useCallback(async () => {
+  const fetchToValidateRoute = async () => {
     setIsLoading(true);
     const req = await fetcher(params);
-    flags.current = {
-      ...flags.current,
-      didValidate: true,
-    };
     setIsLoading(false);
     if (!req.ok) {
-      invalidRoutes.current.push(pathname);
       return setIsValid(false);
     }
     setIsValid(true);
-  }, []);
+  };
   useEffect(() => {
     fetchToValidateRoute();
   }, []);
 
-  useEffect(() => {
-    if (flags.current.didValidate && flags.current.isFirstRender) {
-      flags.current = {
-        ...flags.current,
-        isFirstRender: false,
-      };
-      // Now the user is using client routing instead of trying to tamper with the url ;)
-      setIsValid(true);
-    }
-  }, [pathname]);
   if (isLoading) {
     return <LoadingScreen />;
   }
-  if (
-    (!isValid && flags.current.isFirstRender) ||
-    invalidRoutes.current.includes(pathname)
-  ) {
+  if (!isValid) {
     return <ErrorPage />;
   }
   return children;
-};
+}
